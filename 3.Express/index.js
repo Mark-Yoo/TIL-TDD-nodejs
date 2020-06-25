ccf63e79b8fc45b09569a9af67817beb9422d444
Mark Yoo <yjinhyuk@gmail.com>
2020-06-25 12:31:44 +0900

    Feat: use express

--- 3.Express/index.js
+++ 3.Express/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const users = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Grace" },
+  { id: 3, name: "Mathew" },
+  { id: 4, name: "Sammy" },
+];
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/users", (req, res) => {
+  res.json(users);
 });
 
 app.listen(3000, () => {
