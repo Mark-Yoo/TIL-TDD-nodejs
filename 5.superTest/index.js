b72d767e813209cb9cf2290cb728e63dcac09fee
Mark Yoo <yjinhyuk@gmail.com>
2020-06-26 12:27:59 +0900

    Feat: Apply supertest library

---
+++ 5.superTest/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const users = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Grace" },
+  { id: 3, name: "Mathew" },
+  { id: 4, name: "Sammy" },
+];
+
+app.use(morgan("dev"));
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.listen(3000, () => {
+  console.log("express server is now running");
+});
+
+module.exports = app;
