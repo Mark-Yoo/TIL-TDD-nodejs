71958735c7668de0f89393687997fb0fd9052def
Mark Yoo <yjinhyuk@gmail.com>
2020-06-30 11:36:19 +0900

    Feat: create TDD for POST user example

---
+++ 8.POST_user_example/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+let users = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Grace" },
+  { id: 3, name: "Mathew" },
+  { id: 4, name: "Sammy" },
+];
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/users", (req, res) => {
+  req.query.limit = req.query.limit || 10;
+  const limit = parseInt(req.query.limit, 10);
+  if (Number.isNaN(limit)) {
+    return res.status(400).end();
+  }
+  res.json(users.slice(0, limit));
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id)) return res.status(400).end();
+  const user = users.filter((user) => user.id === id)[0];
+  if (!user) return res.status(404).end();
+  res.json(user);
+});
+
+app.post("/users", (req, res) => {
+  const name = req.body.name;
+  const id = Date.now();
+  const user = { id, name };
+  users.push(user);
+  res.status(201).json(user);
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id)) return res.status(400).end();
+  users = users.filter((user) => user.id !== id);
+  res.status(204).end();
+});
+
+app.listen(3000, () => {
+  console.log("express server is now running");
+});
+
+module.exports = app;
