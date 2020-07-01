f101eeeffac521e2e7636acd246ca53dd75f1c9b
Mark Yoo <yjinhyuk@gmail.com>
2020-07-01 13:03:59 +0900

    Feat: NODE_ENV=test added to package.json

--- 9.Refactoring/index.js
+++ 9.Refactoring/index.js
@@ -4,7 +4,11 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const user = require("./api/user");
 
-app.use(morgan("dev"));
+// process로 들어오는 env.NODE.ENV는 package.json에 있는 NODE_ENV=test 를 받아온다.
+// 테스트 환경에서는 디테일한 로그를 찍지 않도록 만든다.
+if (process.env.NODE_ENV !== "test") {
+  app.use(morgan("dev"));
+}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
